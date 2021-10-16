ae3f3df7a379f75c206870719dfe9c206d0d1de2
David Lin <davidzwlin@gmail.com>
2021-10-16 12:22:06 -0700

    server update

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const contentful = require("contentful");
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('HFB Recipe System Backend')
+});
+
+
+// const client = contentful.createClient({
+//   // This is the space ID. A space is like a project folder in Contentful terms
+//   space: "developer_bookshelf",
+//   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+//   accessToken: "0b7f6x59a0"
+// });
+// // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
+// client
+//   .getEntry("5PeGS2SoZGSa4GuiQsigQu")
+//   .then(entry => console.log(entry))
+//   .catch(err => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
