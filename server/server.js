092fbcb3774a9bc7ac892369dbf44338b08e3101
David Lin <davidzwlin@gmail.com>
2021-10-16 13:52:12 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 
 // const client = contentful.createClient({
 //   // This is the space ID. A space is like a project folder in Contentful terms
-//   space: "developer_bookshelf",
+//   spaceID: "developer_bookshelf",
 //   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
 //   accessToken: "0b7f6x59a0"
 // });
