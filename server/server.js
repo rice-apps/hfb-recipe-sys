62195a9e44a7d428c62338076f9bddcdafb1d6fc
David Lin <davidzwlin@gmail.com>
2021-10-16 14:13:22 -0700

    update variable names

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000
 const accessToken = process.env.ACCESS_TOKEN 
-const spaceID = process.env.SPACE
+const spaceID = process.env.SPACE_ID
 
 app.get('/', (req, res) => {
   res.send('HFB Recipe System Backend')
