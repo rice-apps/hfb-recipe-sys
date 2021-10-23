6438e6cea90b423b968b0451a12301ec74c484eb
Gabby Franklin <70460191+fgabrielle1445@users.noreply.github.com>
2021-10-23 14:46:00 -0500

    Updated JSON output for the recipe data

--- server/README.md
+++ server/README.md
@@ -1,1 +1,6 @@
 # Backend for Houston Food Bank Recipe System
+
+# To deploy:
+```bash
+git subtree push --prefix server heroku master
+```
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000
 const accessToken = process.env.ACCESS_TOKEN 
-const spaceID = process.env.SPACE_ID
+const space_ID = process.env.SPACE_ID
 
 app.get('/', (req, res) => {
   res.send('HFB Recipe System Backend')
@@ -14,19 +14,39 @@ app.get('/', (req, res) => {
 
 const client = contentful.createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
-  space: spaceID,
+  space: space_ID,
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
   accessToken: accessToken
 });
 
 app.get('/recipes', (req, res) => {
-  client.getEntries({
-    order: 'sys.createdAt'
-  })
-  .then(function(entries) {  
+  client.getEntries({order: 'sys.createdAt'})
     // for each item in entries.item, send item.field
-      res.send(entries.items.map(item => item.fields));
-    });
+    .then(data => data.items.map(item => item.fields))
+    // send result
+    .then(recipes => recipes.map(recipe => { 
+      return {
+        ...recipe,
+        photo: recipe.photo.fields.file.url
+      }
+    }))
+    .then(recipes => recipes.map(recipe => {
+      return {
+        ...recipe,
+        ingredientList: recipe.ingredientList.map(entry => {
+          return {
+            ingredient: entry.key,
+            quantity: entry.value
+          }
+        })
+      }
+    }))
+    .then(recipes => res.send(recipes));
+    // // Get image urls
+    // .then(recipes => {
+    //   // Image url at photo -> fields -> url
+    //   return recipes
+    // }).then(data => res.send(data));
 });
 
 app.listen(port, () => {
