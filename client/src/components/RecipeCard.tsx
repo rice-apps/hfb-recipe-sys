24ca2573c4a87c5eef1b021bd5fd7ca6e888d6ed
Chase Hartsell <cmhartsell@me.com>
2021-10-30 14:30:42 -0500

    Made pretty recipe cards

--- client/src/components/RecipeCard.tsx
+++ client/src/components/RecipeCard.tsx
@@ -1,36 +1,97 @@
-import React from 'react'
-import { Card, Row, Col, Typography } from 'antd';
-import styled from 'styled-components';
+import React from 'react';
+import { Row, Col, Typography } from 'antd';
+import { TagsOutlined } from '@ant-design/icons';
 const { Text } = Typography;
 
-const Tag = styled.p`
-    color: darkGray,
-    background-color: gray,
-    padding: 5px,
-    border-radius: 10px
-`;
+const CourseTag = (props: any) => {
+    return (
+        <p
+        style={{
+            color: "#CD5327",
+            backgroundColor: "#CD52371F",
+            borderRadius: "20px",
+            padding: "5px",
+            textAlign: "center",
+            fontStyle: "italic",
+        }}>{props.text}</p>
+    )
+}
+
+const RestrictionTag = (props: any) => {
+    return (
+        <div
+        style={{
+            width: "30px",
+            height: "30px",
+            backgroundColor: props.color,
+            borderRadius: "20px",
+            display: "inline-block",
+            margin: "0px 10px 0px 0px",
+        }}></div>
+    )
+}
 
 export const RecipeCard = (props: any) => {
+    const renderDietaryRestrictions = () => {
+        var rest = [];
+        if(props.glutenFree) {
+            rest.push(<RestrictionTag color="#F1C94D" />);
+        }
+        if(props.vegan) {
+            rest.push(<RestrictionTag color="#3E833D" />);
+        }
+        if(props.vegetarian) {
+            rest.push(<RestrictionTag color="#7EAF53" />);
+        }
+        if(props.nutFree) {
+            rest.push(<RestrictionTag color="#CD5237" />);
+        }
+        return (
+            <div
+            style={{
+                display: "flex",
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'flex-end',
+                marginLeft: "auto",
+            }}>
+                {rest}
+            </div>
+        );
+    }
+
     return (
-        <div>
-            <Card title={props.title}>
-                <img src={props.image} style={{
-                    width: "100%",
-                    height: "500px",
-                }} />
+        <div
+        style={{
+            height: "100%",
+            width: "100%",
+            borderRadius: "50px",
+            border: "1px solid black",
+        }}>
+            <img src={props.image} style={{
+                width: "100%",
+                height: "400px",
+                borderRadius: "50px 50px 0px 0px",
+            }} />
+            <div
+            style={{
+                padding: "20px",
+            }}>
+                <h2
+                style={{
+                    fontStyle: "bold",
+                }}>{props.title}</h2>
                 <Row gutter={[16, 16]}>
-                    <Col span={6}>
-                        <Tag>{props.cuisine}</Tag>
-                    </Col>
                     {props.course.map((c: string) => {
                         return (
-                            <Col span={6}>
-                                <Tag>{c}</Tag>
+                            <Col span={8}>
+                                <CourseTag text={c} />
                             </Col>
                         );
                     })}
                 </Row>
-            </Card>
+                {renderDietaryRestrictions()}
+            </div>
 
             {
             /*
--- client/src/pages/Recipe.tsx
+++ client/src/pages/Recipe.tsx
@@ -1,39 +1,66 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { RecipeCard } from '../components/RecipeCard';
 import { useParams } from 'react-router-dom';
 import { Card, Typography } from 'antd';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Row, Col, Input, AutoComplete, Dropdown, Button, Menu } from 'antd';
+
+
 const { Text } = Typography;
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 export default function Recipe(props: any) {
     const { id } = useParams<{ id: string }>();
-    const recipe = props.recipes[id];
+
+    const recipe = (props.recipes.length > 0) ? props.recipes[id]: {};
 
     return (
-        
-        <Card title={recipe.title}>
-            {console.log(recipe)}
-        <img src={recipe.image} />
-        <Text type="secondary">{recipe.cuisine}</Text>
-        {recipe.course.map((c: string) => {
+        <>
+        <h1>{recipe.title}</h1>
+        <Row gutter={[8, 16]}>
+        <Col span={8}> <b>Servings</b>: {recipe.servings} </Col>
+        <Col span={8}> <b> Calories</b>: {recipe.calories}</Col>
+        <Col span={8} />
+        </Row>
+
+        <Row gutter={[16, 16]}>
+        <Col span={12}>  <img src={recipe.photo} />  </Col>
+        <Col span={12}> <h2>Ingredients</h2>
+        {recipe.ingredientList && recipe.ingredientList.map((dict: any) => {
             return (
-                <Text type="secondary">{c}</Text>
+                <p> - {dict["quantity"]} {dict["ingredient"]}</p>
+            );
+        })}</Col>
+
+        <Col span={12}> <h1> Tags: </h1>
+<Text type="secondary">{recipe.cuisine}</Text>
+        {recipe.course &&
+        recipe.course.map((c: string) => {
+            return (
+                <Text type="secondary"> {c}</Text>
             );
         })}
-        <b>Servings</b>: {recipe.servings}
-        <b>Calories</b>: {recipe.calories}
-        <h2>Ingredients</h2>
-        {recipe.ingredientList.map((dict: any) => {
-            return (
-                <p> - {dict["value"]} {dict["key"]}</p>
-            );
-        })}
-        <h2>Instructions</h2>
-        {recipe.instructions.split("\n").map((instr: string) => {
+ </Col>
+        <Col span={12}> <h2>Instructions</h2>
+        {recipe.instructions && recipe.instructions.split("\n").map((instr: string) => {
             return (
                 <p>{instr}</p>
             );
-        })}
-    </Card>
-    
+        })}  </Col>
+        </Row>
+
+
+
+        </> 
         )
 }
