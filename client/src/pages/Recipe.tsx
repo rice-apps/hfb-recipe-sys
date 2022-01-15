07617366a17926099b21dc12c4bd767d80468004
Shourya Munjal <35500239+shouryam@users.noreply.github.com>
2022-01-15 11:10:37 -0800

    commented recipe page

--- client/src/pages/Recipe.tsx
+++ client/src/pages/Recipe.tsx
@@ -29,14 +29,22 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
 }));
 
+/*
+    Props are the list of recipes 
+*/
 export default function Recipe(props: {recipes: RecipeData[]}) {
+    //Get the ID from the URL for this specific recipe
     const { id } = useParams<{ id: string }>();
+    //Define the reference to the printable component 
     const printComponentRef = useRef<HTMLDivElement>(null);
 
+    //Set the recipe to be the one in the List with the ID from the URL
     const recipe = props.recipes.find(recipe => recipe.id === id)
 
+    //All us to have a back button
     let history = useHistory();
 
+    //Function to render the 4 dietary restriction tags 
     const renderDietaryRestrictions = () => {
         if (!recipe) return null;
 
@@ -64,46 +72,55 @@ export default function Recipe(props: {recipes: RecipeData[]}) {
         );
     }
 
-
+    //Main Rendered component checks if the recipes have been found before loading anything
     return ( recipe === undefined ? <></> :
         <>
+
             <div className="header_bar">
                 <header>
-                  
+                    {/* forms the green header bar seen*/}
                     <Row className="top_row">
+                        {/* Go back button using the Icons from semantic UI*/}
                         <Col className="gutter-row" flex={1} onClick={() => history.goBack()}>
                             <Icon name='angle left' className="back_button" size="large" inverted />
                         </Col>
                         <Col className="gutter-row" flex={20}>
+                            {/* Main title*/}
                             <div className="name_header">{recipe.title}</div>
                         </Col>
                         <Col className="gutter-row" flex={1}>
 
                         </Col>
                     </Row>
-
+                        {/* Sub information to title on the next row underneath */}
+ 
                     <Row gutter={8} className="info_row" justify="center">
                         <Col className="gutter-row" span={3} >
+                             {/* Cusine Type */}
                             <div className="detail_header" >Cusine</div>
                             <div className="information_header">{recipe.cuisine}</div>
                         </Col>
                         <Col>
+                             {/* Bar */}
                             <div className="vl"></div>
                         </Col>
                         <Col className="gutter-row" span={3}>
+                             {/* Serving Size */}
                             <div className="detail_header" >Serving Size</div>
                             <div className="information_header">{recipe.servings}</div>
 
                         </Col>
                         <Col>
+                         {/* Bar */}
                             <div className="vl"></div>
                         </Col>
                         <Col className="gutter-row" span={3}>
-
+                             {/* Calories  */}
                             <div className="detail_header" >Calories</div>
                             <div className="information_header">{recipe.calories}</div>
                         </Col>
                         <Col className="gutter-row" span={3}>
+                             {/* Print Button */}
                             <ReactToPrint
                                 trigger={() =>
                                     <div className="print_button">                <Icon name='print' size='large' color='green' />
@@ -112,6 +129,7 @@ export default function Recipe(props: {recipes: RecipeData[]}) {
                                 content={() => printComponentRef.current}
                                 documentTitle="AwesomeFileName"
                             />
+                             {/* Prints from the print div below*/}
 
                         </Col>
                     </Row>
@@ -125,6 +143,7 @@ export default function Recipe(props: {recipes: RecipeData[]}) {
                             <div className="restrictions">
                                 {renderDietaryRestrictions()}
                             </div>
+                             {/* Ingreident List*/}
                             <div className="ingredients">
                                 <h2>Ingredients</h2>
                                 <ul className="ingredientsList">
@@ -137,11 +156,13 @@ export default function Recipe(props: {recipes: RecipeData[]}) {
                             </div>
                         </div>
                     </div>
+                     {/* Photo of the recipe */}
                     <div className="photoContainer">
                         <img src={recipe.photo} className="image" />
                     </div>
                 </div> 
             </div>
+             {/* Intstructions*/}
             <div className="instructionsContainer">
                     <h2>Instructions</h2>
                     {recipe.instructions && recipe.instructions.split("\n").map((instr: string) => {
@@ -153,20 +174,22 @@ export default function Recipe(props: {recipes: RecipeData[]}) {
                 </div>
 
 
-
+             {/* What is being printed -> technically on the page but display is None meaning it wont show up*/}
             <div style={{ display: "none" }}>
-
+                 {/*What gets printed  */}
                 <div ref={printComponentRef}>
+                     {/*Title */}
                     <div style={{ height: "50vh", width: "95vw" }}>
                         <h1>{recipe.title}</h1>
                         <Row gutter={[8, 16]}>
-
+                             {/* Ingreidents*/}
                             <Col span={8} flex={"flex"} style={{ height: "40vh", width: "95vw" }}> <h2>Ingredients</h2>
                                 {recipe.ingredientList && recipe.ingredientList.map((dict: any) => {
                                     return (
                                         <p style={{ margin: "2mm" }}> - {dict["quantity"]} {dict["ingredient"]}</p>
                                     );
                                 })}</Col>
+                             {/* Instructions*/}
                             <Col span={16} flex={"flex"} style={{ height: "40vh", width: "95vw" }}> <h2>Instructions</h2>
                                 {recipe.instructions && recipe.instructions.split("\n").map((instr: string) => {
                                     return (
@@ -175,8 +198,9 @@ export default function Recipe(props: {recipes: RecipeData[]}) {
                                 })}  </Col>
                         </Row>
                     </div>
-
+                     {/* Line */}
                     <hr></hr>
+                     {/* Repeat of top half*/}
                     <div style={{ height: "50vh", width: "95vw" }}>
                         <h1>{recipe.title}</h1>
                         <Row gutter={[8, 16]}>
