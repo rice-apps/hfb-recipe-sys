f18e78d12b8fee64435821ccf00e1fca41861bc3
Gabby <fgabrielle1445@yahoo.com>
2022-01-15 13:03:00 -0700

    Separated the Filter and Search components into separate files Still need to - Fix errors in both - Delete on the main page - And figure out imports

---
+++ client/src/components/main/Filter.tsx
@@ -0,0 +1,61 @@
+import React, { useState, ChangeEvent } from 'react';
+import { Input, Button, Checkbox } from 'antd';
+
+function Filter( props: { recipes: RecipeData[]} ) {
+    const [checkedFilters, setCheckedFilters] = useState<string[]>([]); // State where the checked dietary restrictions tags are stored
+
+    /**
+   * Saves the new filter state
+   */
+  function onFilterChange(list: CheckboxValueType[]) {
+    setCheckedFilters(list as string[]);
+  };
+    /**
+   * Returns the recipes after applying the filters (e.g. vegetarian, gluten-free, ...)
+   */
+  function getFilteredRecipes(recipes: RecipeData[]): RecipeData[] {
+    return recipes.filter(recipe => {
+      // Makes a tag list of the dietary restrictions options that each recipe has
+      const tags: String[] = []
+      if (recipe.glutenFree) {
+        tags.push("Gluten-Free");
+      }
+      if (recipe.vegetarian) {
+        tags.push("Vegetarian");
+      }
+      if (recipe.vegan) {
+        tags.push("Vegan");
+      }
+      if (recipe.nutFree) {
+        tags.push("Nut-Free");
+      }
+      // Checks that the current recipe have every tag that is checked in its tags list
+      return checkedFilters.every(i => tags.includes(i));
+    })
+  }
+
+  return(
+    <div className="filterContainer">
+            <div className="filterTitle">
+                {filterIcon}
+                <h1 style={{"marginBottom": "20px", "marginLeft": "10px"}}>Filter Results</h1>
+            </div>
+            <div className="checkboxTitle">
+              <p>DIETARY RESTRICTIONS</p>
+            </div>
+
+            <div className="checkboxContainer">
+              {/** Creates the checkbox display on the main page */}
+              <CheckboxGroup options={plainOptions} value={checkedFilters} onChange={onFilterChange} />
+            </div>
+            <div className="filterBottom">
+              {/** Filter reset button, clears the current values in the checkedFilters state */}
+                <Button type="default" onClick={() => setCheckedFilters([])}>
+                    Reset Filters
+                </Button>
+            </div>
+          </div>
+);
+}
+
+export default Filter;
