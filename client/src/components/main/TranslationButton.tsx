0e8874c3491c43d46c2fa57b60dcb03e228c8abb
Gabby <fgabrielle1445@yahoo.com>
2022-01-22 12:56:00 -0600

    Made a TranslationButton class to store the translation switch. Will likely have to either create copies of the backend for readouts or create multiple language divided objects there for the draw

---
+++ client/src/components/main/TranslationButton.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useTranslation } from "react-i18next";
+import { Menu, Dropdown, message } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+
+// import { i18n } from './translations/i18n';
+
+
+function TranslationButton() {
+
+    const [language, setLanguage] = useState('en');
+
+    const { t } = useTranslation();
+
+
+    const onClick = ({ key }) => {
+        message.info(`Click on item ${key}`);
+    };
+
+    const menu = (
+        <Menu onClick={onClick}>
+          <Menu.Item key="en">English</Menu.Item>
+          <Menu.Item key="sp">Spanish</Menu.Item>
+          <Menu.Item key="vi">Vietnamese</Menu.Item>
+        </Menu>
+    );
+
+    return(
+        <div>
+            <Dropdown overlay={menu}>
+                <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                    Select Language <DownOutlined />
+                </a>
+            </Dropdown>
+            
+        </div>
+    )
+
+}
+
+export default TranslationButton;
