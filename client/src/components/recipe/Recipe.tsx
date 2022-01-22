4132a3238862efa0952b682e6de8a4c4effd44ee
Chase Hartsell <cmhartsell@me.com>
2022-01-22 14:46:00 -0600

    Align ingredients with instructions

--- client/src/components/recipe/Recipe.tsx
+++ client/src/components/recipe/Recipe.tsx
@@ -9,33 +9,6 @@ import CourseTags from '../shared/CourseTags';
 
 
 export const Recipe = (props: { recipe: RecipeData }) => {
-    /*Function to render the 4 dietary restriction tags 
-    const renderDietaryRestrictions = () => {
-        if (!props.recipe) return null;
-
-        var rest = [];
-        if (props.recipe.glutenFree) {
-            rest.push(
-                <div className="restTag">
-                    <span style={{ color: "#F1C94D" }}>Gluten-free</span>
-                    <RestrictionTag color="#F1C94D" icon="leaf" />
-                </div>);
-        }
-        if (props.recipe.vegan) {
-            rest.push(<RestrictionTag color="#3E833D" icon="leaf" />);
-        }
-        if (props.recipe.vegetarian) {
-            rest.push(<RestrictionTag color="#7EAF53" icon="leaf" />);
-        }
-        if (props.recipe.nutFree) {
-            rest.push(<RestrictionTag color="#CD5237" icon="leaf" />);
-        }
-        return (
-            <div className="recipeRestTagContainer">
-                {rest}
-            </div>
-        );
-    }*/
     return (
         <div>
             <div className="recipeDataContainer">
