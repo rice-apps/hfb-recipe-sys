5731ef212deeaf378b15033edc259638312bad9e
Ryan Knightly <reknightly@gmail.com>
2022-01-29 15:56:06 -0600

    Convert to function component, move listener creation to googleTranslateElementInit

---
+++ client/src/components/main/GoogleTranslate.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useRef } from 'react';
+
+import './googleElement.css';
+
+/**
+ * A component with a Google Translate dropdown that allows selecting a language for the page's HTML to be translated to
+ */
+function GoogleTranslate() {
+
+    /**
+     * A reference to the div containing the google translate language-selection dropdown
+     */
+    const googleTranslateDiv = useRef<HTMLDivElement>(null);
+
+    /**
+     * Initializes the google translate element
+     */
+    function googleTranslateElementInit () {
+        // Create google translate element
+        new (window as any).google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'en,es,vi,zh-CN,ar', layout: (window as any).google.translate.TranslateElement.FloatPosition.TOP_LEFT}, 'google_translate_element')
+
+        // Set up listener to capture language selection
+        if(googleTranslateDiv.current) {
+            let selector = googleTranslateDiv.current.getElementsByClassName("goog-te-combo")[0] as HTMLSelectElement
+            if(selector) {
+                // TODO: update state on language selection
+                console.log("Adding listener");
+                selector.addEventListener("change", (e) => console.log("Selected Language:", selector.value), false)
+            } else {
+                console.log("Selector not defined");
+            }
+        }
+    }
+
+    /**
+     * Adds the google translate source to the page
+     */
+    useEffect(() => {
+        var addScript = document.createElement('script');
+        addScript.setAttribute('src', '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit');        
+        document.body.appendChild(addScript);  
+        (window as any).googleTranslateElementInit = googleTranslateElementInit;   
+    }, [])
+
+    /**
+     * Return a div to contain the language selection dropdown
+     */
+    return (
+        <div id="google_translate_element" ref={googleTranslateDiv}></div>
+    );
+
+}
+
+export default GoogleTranslate;
