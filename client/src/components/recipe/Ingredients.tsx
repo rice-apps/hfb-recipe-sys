978d0f3ff3eae6ff6bb12d1c1862d98649ac8ff7
Chase Hartsell <cmhartsell@me.com>
2022-02-05 14:21:43 -0600

    Serving change work with ranges

--- client/src/components/recipe/Ingredients.tsx
+++ client/src/components/recipe/Ingredients.tsx
@@ -6,7 +6,20 @@ import Fraction from 'fraction.js';
 
 export const Ingredients = (props: { recipe: RecipeData, scale: number } ) => {
     const formatFraction = (frac: string, sc: number) => {
-        var f = new Fraction(frac);
+        if(frac == "0") {
+            return "";
+        }
+
+        console.log(frac);
+
+        var ff = frac.trim().split('-');
+        console.log(ff);
+        var f = new Fraction(ff[0]);
+        if(ff.length > 1) {
+            var f2 = new Fraction(ff[1]);
+            return f.mul(sc).toFraction(true) + " - " + f2.mul(sc).toFraction(true);
+        }
+
         return f.mul(sc).toFraction(true);
     }
 
