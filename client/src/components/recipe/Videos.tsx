36d32581c303adb40dd10f9b6a8d91072f34ffb4
Emily Huang <71594652+emilyhtx14@users.noreply.github.com>
2022-02-19 13:18:32 -0600

    completed videos

--- client/src/components/recipe/Videos.tsx
+++ client/src/components/recipe/Videos.tsx
@@ -4,31 +4,18 @@ import '../../style/Recipe.css';
 import ReactPlayer from 'react-player';
 
 export const Videos = (props: { recipe: RecipeData } ) => {
-    // const videoExists = 
-    return(
-        <div className="videosContainer">
-                <h2></h2>
-                {props.recipe.videoLink}
-                hello
-               <ReactPlayer url = {props.recipe.videoLink} />
-               {/* <ReactPlayer url = "https://www.youtube.com/watch?v=9OGAJ5Y5GCg"/> */}
+    return( 
+        <div>
+            {props.recipe.videoLink && 
+                <div>   
+                    <div className="videosContainer">
+                        <h2></h2>
+                        <ReactPlayer url = {props.recipe.videoLink} />
+                    </div>
+                </div>
+            }
         </div>
-
-    //    <div>
-            
-    //         {/* {
-    //             {{props.recipe.videoLink} != ""
-    //                 <div className="videosContainer">
-    //                     <h2></h2>
-    //                     <ReactPlayer
-    //                         url = {props.recipe.videoLink}
-    //                     />
-    //                 </div>
-    //             }
-    //         } */}
-            
-    //     </div>
-        )
-    }
+    )
+ }
 
 
