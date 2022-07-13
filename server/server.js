2f6655d3f6f21b376929b8c093c83f2152d9e2b8
Maggie Ku <yk58@rice.edu>
2022-07-12 20:42:06 -0500

    User Auth Attempt

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const contentful = require('contentful');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 
 // Load environment variables from .env
 require('dotenv').config();
@@ -10,11 +13,12 @@ require('dotenv').config();
 const app = express();
 
 // Define Constants
-const port = process.env.PORT || 8000;
-const accessToken = process.env.ACCESS_TOKEN; 
+const port = process.env.PORT || 3000;
+const accessToken = process.env.ACCESS_TOKEN;
 const space_ID = process.env.SPACE_ID;
 
 // Allow cross-origin resource sharing
+app.use(bodyParser.json());
 app.use(cors());
 
 // Define home route to confirm backend is working
@@ -22,19 +26,37 @@ app.get('/', (req, res) => {
   res.send('HFB Recipe System Backend')
 });
 
+console.log(process.env.REACT_APP_CLIENT_ID)
 // Create authenticated contentful client
 const client = contentful.createClient({
   space: space_ID,
   accessToken: accessToken
 });
 
+//import routes
+const authRoutes = require('./User Auth/routes/auth');
+const { db } = require('./User Auth/models/User');
+
+// db
+mongoose
+  .connect(process.env.DATABASE, {
+    //useNewUrlParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB Connected'));
+
+
+//routes middleware
+app.use('/api', authRoutes);
+
 // Define /recipes endpoint to return a list of all recipes in contentful
 app.get('/recipes', (req, res) => {
-  client.getEntries({order: 'sys.createdAt'})
+  client.getEntries({ order: 'sys.createdAt' })
     // Extract fields from each contentful object
     .then(data => data.items.map(item => item.fields))
     // Extract url from photo data
-    .then(recipes => recipes.map(recipe => { 
+    .then(recipes => recipes.map(recipe => {
       return {
         ...recipe,
         photo: recipe.photo.fields.file.url
@@ -55,6 +77,7 @@ app.get('/recipes', (req, res) => {
     // Send processed recipes to client
     .then(recipes => res.send(recipes));
 });
+
 
 // Run app at specified port
 app.listen(port, () => {
